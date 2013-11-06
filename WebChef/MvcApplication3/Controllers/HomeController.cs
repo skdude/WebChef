b30c3430292d60843385056ba5a7fb7ea88a1027
Maarten Hoste <Maarten.Hoste@student.pxl.be>
2013-11-06 12:57:14 +0100

    Blabla

---
+++ WebChef/MvcApplication3/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About";
+            return View();
+        }
+
+        public ActionResult Recepten()
+        {
+            ViewBag.Title = "Recepten";
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Title = "Login";
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Title = "Register";
+            return View();
+        }
+
+        public ActionResult Sterrengerechten()
+        {
+            ViewBag.Title = "Sterrengerechten";
+            return View();
+        }
+    }
+}
